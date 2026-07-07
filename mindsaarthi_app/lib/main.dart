5ccfc5629fd4bac4059d743552ad51461aac6e2b
b22me012 <anshhbansal03+github@gmail.com>
2026-07-07 15:10:27 +0530

    Add GitHub Pages deployment

--- mindsaarthi_app/lib/main.dart
+++ mindsaarthi_app/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mindsaarthi_app/firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
